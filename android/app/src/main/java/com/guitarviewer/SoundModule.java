0538b1ccdf5737560d64f3fa35a7a2e83d218831
Michel Calheiros <michel@calheiros.dev>
2022-01-24 16:05:42 -0300

    feat: WIP - added android SoundModule, updated dependencies

---
+++ android/app/src/main/java/com/guitarviewer/SoundModule.java
@@ -0,0 +1,75 @@
+package com.guitarviewer;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Handler;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+// https://stackoverflow.com/questions/2413426/playing-an-arbitrary-tone-with-android
+public class SoundModule extends ReactContextBaseJavaModule {
+    // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
+    // and modified by Steve Pomeroy <steve@staticfree.info>
+    private final int duration = 1; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
+
+    @Override
+    public String getName() {
+        return "SoundModule";
+    }
+
+    void genTone(double freqOfTone) {
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound() {
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
+    }
+
+    @ReactMethod
+    void playSoundInFrequency(double frequency) {
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone(frequency);
+                handler.post(new Runnable() {
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+}
