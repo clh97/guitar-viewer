7e74193b696be3b9862b595ada36dd76ddfc6d90
Michel Calheiros <michel@calheiros.dev>
2022-01-24 18:25:06 -0300

    feat: added working java SoundModule

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import utils from '../config/utils';
+import React from "react";
+import { NativeModules, View, Text, Button } from "react-native";
 
-import constants from '../config/const';
+import utils from "../config/utils";
+
+import constants from "../config/const";
 
 const Home = (props) => {
-    const { navigation } = props;
+  const { navigation } = props;
+  const { SoundModule } = NativeModules;
 
-    React.useEffect(() => {
+  React.useEffect(() => {
+    (async () => {
+      const collections = await utils.collections.retrieveSavedCollections();
 
-        (async () => {
-            const collections = await utils.collections.retrieveSavedCollections();
+      if (!collections) {
+        await utils.collections.initCollection();
+      }
+    })();
+  }, []);
 
-            if(!collections) {
-                await utils.collections.initCollection();
-            }
-         })();
-    }, []);
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        height: constants.height(),
+      }}
+    >
+      <Button onPress={() => navigation.navigate("Scale")} title="Scale" />
+      <Button
+        onPress={() => {
+          SoundModule.playSoundInFrequency(440);
+        }}
+        title="Sound"
+      />
+    </View>
+  );
+};
 
-    return (
-        <View style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: constants.height()
-        }}>
-            <Button onPress={() => navigation.navigate('Scale')} title="Scale" />
-        </View>
-    )
-}
-
-export default Home
+export default Home;
